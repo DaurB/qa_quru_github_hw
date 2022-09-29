e436a3eb1e5a9ffa61cd5610623a771167499ccb
dauren <biboldauren@gmail.com>
2022-09-30 01:57:46 +0600

    second

---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class test {
+
+    @Test
+    void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
+    }
+}
